9a1034d62aad4ce136582705e1e8eab441a56d4c
Pagan VIII <pagan@skynet.ie>
2015-06-19 06:30:58 +0000

    Fixed broken /beers SQL query with a new one

--- express-app/brew.js
+++ express-app/brew.js
@@ -43,21 +43,7 @@ app.get('/', function (req, res) {
 app.get('/beers', function(req, res) {
   res.type('json');
 
-  /*
-  I think something like this will work instead:
-  
-  SELECT * FROM beers LEFT JOIN ON (
-    SELECT likes.beer_id, count(*) AS tastes
-    FROM likes
-    WHERE likes.like_type = 1
-    GROUP BY likes.beer_id
-    ) tastes ON tastes.beer_id = beers.id;
-
-    The subquery counts all the tastes for each beer and
-    then we just join it with the beers table based on id.
-
-   */
-  var sql = "SELECT *,COUNT(likes.beer_id) AS taste_count FROM beers LEFT JOIN likes ON beers.id = likes.beer_id WHERE likes.like_type = 1 OR likes.like_type IS NULL GROUP BY likes.beer_id";
+  var sql = 'select *,(select count(*) from likes where beer_id = beers.id and likes.like_type = 1) as "taste_count",(select count(*) from likes where beer_id = beers.id and likes.like_type = 2) as "favorite_count" from beers;';
 
   connection.query(sql, function(err, rows) {
     res.json({ beers: rows });
@@ -68,7 +54,7 @@ app.get('/beers', function(req, res) {
 app.get('/establishments', function (req, res) {
   res.type('json');
 
-  var sql = "select establishments.*, statuses.* FROM establishments, statuses WHERE establishments.id = statuses.establishment_id ORDER BY establishments.id;"
+  var sql = "select establishments.*, statuses.* FROM establishments, statuses WHERE establishments.id = statuses.establishment_id ORDER BY establishments.id;";
 
   connection.query(sql, function(err, rows) {
         var prow = [];
