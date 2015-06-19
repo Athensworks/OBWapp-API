349779a88f81cf69b3732fd4e30bc6d6c1b8c922
Pagan VIII <pagan@skynet.ie>
2015-06-19 05:50:32 +0000

    Added rate_beer_id to beers table, fixed formatting so it doesn't look strange!

--- create_tables.sql
+++ create_tables.sql
@@ -1,34 +1,35 @@
 create table establishments(
-id int unsigned not null primary key
-,name varchar(100)
-,lat  float
-,lon float
-,address varchar(100)
+id int unsigned not null primary key,
+name varchar(100),
+lat  float,
+lon float,
+address varchar(100)
 );
 
 create table beers(
-id int unsigned not null primary key
-,name varchar(100)
-,brewery varchar(100)
-,ibu smallint unsigned
-,abv float
-,limited_release bool
-,description text
+id int unsigned not null primary key,
+name varchar(100),
+brewery varchar(100),
+ibu smallint unsigned,
+abv float,
+limited_release bool,
+description text,
+rate_beer_id varchar(64)
 );
 
 create table likes(
 id int unsigned not null primary key,
-device_guid varchar(255)
-,beer_id int unsigned
-,age tinyint unsigned
-,like_type tinyint unsigned
-,like_status bool
+device_guid varchar(255),
+beer_id int unsigned,
+age tinyint unsigned,
+like_type tinyint unsigned,
+like_status bool
 );
 
 create table statuses(
-establishment_id int unsigned
-,beer_id int unsigned
-,status tinyint unsigned
-,reported_out_count int unsigned
-,last_out_update datetime
+establishment_id int unsigned,
+beer_id int unsigned,
+status tinyint unsigned,
+reported_out_count int unsigned,
+last_out_update datetime
 );
