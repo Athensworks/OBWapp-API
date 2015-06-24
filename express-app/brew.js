62aadf127d6eaec2a36af0e8ddc20ff8e1d132fe
Pagan VIII <pagan@skynet.ie>
2015-06-24 19:25:00 +0000

    Added delete option with strict matching to establishments

--- express-app/brew.js
+++ express-app/brew.js
@@ -303,6 +303,26 @@ app.put('/admin/establishments', function (req, res) {
   });
 });
 
+app.delete('/admin/establishments', function (req, res) {
+  var estid   = req.body.id;
+  var estname = req.body.name;
+  var estaddr = req.body.address;
+  var estlon  = req.body.lon;
+  var estlat  = req.body.lat;
+
+  var sql = "DELETE FROM establishments WHERE id = ? AND name = ? AND address = ? AND lon = ? AND lat = ? LIMIT 1";
+  var inserts = [estid, estname, estaddr, estlon, estlat];
+  sql = mysql.format(sql, inserts);
+
+  connection.query(sql, function(err, result) {
+    if (result.length == 1) {
+	res.sendStatus(200);
+    } else {
+	res.sendStatus(404);
+    }
+  });
+});
+
 app.post('/admin/beer', function (req, res) {
   res.sendStatus(200);
 });
