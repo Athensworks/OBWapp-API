ba3c26b7e1df5d3b4a5d848f690f214231fc5ff2
Ricky Chilcott <ricky@rbsavvy.com>
2015-06-29 10:51:11 -0400

    Removing testing results and modifying script to a) be executable directly b) run both test files and c) log them to a *.log file

--- Testing/Chakram/TestAPI.sh
+++ Testing/Chakram/TestAPI.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 current_time=$(date "+%m.%d-%H.%M.%S")
-fileName=TestResults-$current_time.txt
-mocha ./PublicAPI.js > $fileName
-mocha ./AdminAPI.js >> $fileName
+fileName=TestResults-$current_time.log
+touch $fileName
+
+echo "Running Chakram and logging tests to $fileName"
+mocha ./PublicAPI.js ./AdminAPI.js | tee $fileName
